f9ef263b17177b46f6ded3caa2399bc3bf29483f
ak-ak-k <adityakirtane27@gmail.com>
2023-04-20 16:05:21 +0530

    update

--- src/app/components/newDesignScreens/loyaltyform/loyaltyform.component.ts
+++ src/app/components/newDesignScreens/loyaltyform/loyaltyform.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-loyaltyform',
   templateUrl: './loyaltyform.component.html',
-  styleUrls: ['./loyaltyform.component.scss']
+  styleUrls: ['./loyaltyform.component.scss'],
 })
 export class LoyaltyformComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
